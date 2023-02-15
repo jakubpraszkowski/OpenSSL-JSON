7711f3214ec767e2b0ae491b361233f2329bf05b
Jakub Praszkowski <95052844+jakubpraszkowski@users.noreply.github.com>
2023-02-15 20:26:43 +0100

    Add files via upload

---
+++ Klient.cpp
@@ -0,0 +1,220 @@
+﻿#pragma comment(lib, "libcurl_imp.lib")
+#pragma comment(lib, "jsoncpp.lib")
+
+#include <curl/curl.h>
+#include <json/json.h>
+#include <iostream>
+#include <winsock2.h> 
+#include <Windows.h> 
+#include <Ws2tcpip.h> 
+
+#include <openssl/sha.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#pragma comment(lib, "ws2_32.lib")
+#include <iostream>
+
+using namespace std;
+
+static size_t Writeback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    size_t rozmiar;
+    rozmiar = size * nmemb;
+    ((std::string*)userp)->append((char*)contents, rozmiar);
+    return rozmiar;
+}
+
+int create_socket(int port)
+{
+    WSADATA wsaData;
+
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        std::cout << "error WSAStartup" << WSAGetLastError() << std::endl;
+        return 0;
+    }
+
+    int s;
+    struct sockaddr_in addr;
+    memset(&addr, '\0', sizeof addr);
+    addr.sin_family = AF_INET;
+    int result;
+    result = inet_pton(AF_INET, "127.0.0.1", &(addr.sin_addr));
+    if (!result) {
+        perror("Unable to make a conversion");
+        return 0;
+    }
+    addr.sin_port = htons(port);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        perror("Unable to create socket");
+        WSACleanup();
+        return 0;
+    }
+
+    if (connect(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("Unable to connect");
+        closesocket(s);
+        WSACleanup();
+        return 0;
+    }
+
+    return s;
+}
+
+SSL_CTX* create_context()
+{
+    const SSL_METHOD* method;
+    SSL_CTX* ctx;
+
+    //OPENSSL_init_crypto();
+
+    method = TLS_client_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+
+    return ctx;
+}
+
+void our_cleanup(SSL* ssl, CURL* curl, SSL_CTX* ctx, int sock) {
+    SSL_free(ssl);
+    curl_easy_cleanup(curl);
+    SSL_CTX_free(ctx);
+    closesocket(sock);
+    WSACleanup();
+}
+
+int main(void) {
+    char buf[1024];
+    string odebrane;
+    int bytes;
+    int sock;
+    SSL_CTX* ctx;
+    SSL* ssl;
+    CURL* curl;
+    CURLcode res;
+    std::string str;
+    Json::Reader reader;
+    Json::Value js;
+    char* znak;
+
+
+    ctx = create_context();
+    if (!ctx) {
+        perror("Error when creating context");
+        exit(EXIT_FAILURE);
+    }
+
+    sock = create_socket(8080);
+    if (!sock) {
+        perror("Error creating socket"); // ???
+        SSL_CTX_free(ctx);
+        exit(EXIT_FAILURE);
+    }
+    ssl = SSL_new(ctx);
+    if (!ssl) {
+        perror("Error when creating SSL"); // ???
+        SSL_CTX_free(ctx);
+        closesocket(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    int resull;
+    resull = SSL_set_fd(ssl, sock);
+    if (!resull) {
+        perror("Error when connecting SSL object with a file descriptor");
+        SSL_free(ssl);
+        SSL_CTX_free(ctx);
+        closesocket(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.open-meteo.com/v1/forecast?latitude=50.33&longitude=19.13&current_weather=true");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Writeback);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &str);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+        {
+            fprintf(stderr, "Curle no okie dokie: %s\n", curl_easy_strerror(res));
+            our_cleanup(ssl, curl, ctx, sock);
+            exit(1);
+        }
+
+        else
+        {
+            if (reader.parse(str, js))
+            {
+                std::cout << "City:\t\tBendzin" << std::endl;
+                if (js.get("latitude", NAN).isDouble()) {
+                    std::cout << "Latitude:\t" << js.get("latitude", NAN).asDouble() << std::endl;
+                }
+                if (js.get("longitude", NAN).isDouble()) {
+                    std::cout << "Longitude:\t" << js.get("longitude", NAN).asDouble() << std::endl;
+                }
+                std::cout << "Time zone:\t" << js.get("timezone", "null") << std::endl;
+
+
+                if (js.get("current_weather", "null").isObject()) {
+                    if (js.get("current_weather", "null").get("temperature", NAN).isDouble()) {
+                        std::cout << "Temperature:\t" << js.get("current_weather", "null").get("temperature", NAN).asDouble() << std::endl;
+                    }
+                    if (js.get("current_weather", "null").get("windspeed", NAN).isDouble()) {
+                        std::cout << "Wind speed:\t" << js.get("current_weather", "null").get("windspeed", NAN).asDouble() << std::endl;
+                    }
+                    if (js.get("current_weather", "null").get("winddirection", NAN).isDouble()) {
+                        std::cout << "Wind direction:\t" << js.get("current_weather", "null").get("winddirection", NAN).asDouble() << std::endl;
+                    }
+                    if (js.get("current_weather", "null").get("weathercode", NAN).isDouble()) {
+                        std::cout << "Weather code:\t" << js.get("current_weather", "null").get("weathercode", NAN).asDouble() << std::endl;
+                    }
+                    std::cout << "Time:\t\t" << js.get("current_weather", "null").get("time", "null") << std::endl;
+                }
+
+                if (SSL_connect(ssl) < 0)
+                {
+                    ERR_print_errors_fp(stderr);
+                    our_cleanup(ssl, curl, ctx, sock);
+                    exit(1);
+                }
+
+                else
+                {
+                    printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
+
+                    if (SSL_write(ssl, str.c_str(), str.length()) <= 0)
+                    {
+                        printf("Blad w wysylaniu write 1");
+                        ERR_print_errors_fp(stderr);
+                        SSL_shutdown(ssl);
+                        our_cleanup(ssl, curl, ctx, sock);
+                        exit(1);
+                    }
+
+                    SSL_shutdown(ssl);
+                    SSL_free(ssl);
+                }
+            }
+            curl_easy_cleanup(curl);
+        }
+
+    }
+
+    SSL_CTX_free(ctx);
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+
+}
---
+++ Serwer.cpp
@@ -0,0 +1,191 @@
+﻿#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include <winsock2.h> 
+#include <Windows.h> 
+#include <Ws2tcpip.h> 
+
+#include <openssl/sha.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+int create_socket(int port)
+{
+    WSADATA wsaData;
+
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        std::cout << "error WSAStartup" << WSAGetLastError() << std::endl;
+        return NULL;
+    }
+
+    int s;
+    struct sockaddr_in addr;
+    memset(&addr, '\0', sizeof addr);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s < 0) {
+        perror("Unable to create socket");
+        WSACleanup();
+        return NULL;
+    }
+
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("Unable to bind");
+        closesocket(s);
+        WSACleanup();
+        return NULL;
+    }
+
+    if (listen(s, 1) < 0) {
+        perror("Unable to listen");
+        closesocket(s);
+        WSACleanup();
+        return NULL;
+    }
+
+    return s;
+}
+
+SSL_CTX* create_context()
+{
+    const SSL_METHOD* method;
+    SSL_CTX* ctx;
+
+    method = TLS_server_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+
+    return ctx;
+}
+
+bool configure_context(SSL_CTX* ctx)
+{
+    if (SSL_CTX_use_certificate_file(ctx, "my.crt", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        return false;
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "my.pass.key", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        return false;
+    }
+
+    if (SSL_CTX_load_verify_locations(ctx, "my.crt", nullptr) <= 0)
+    {
+        ERR_print_errors_fp(stderr);
+        return false;
+    }
+
+    // Dodanie weryfikacji kluczy
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, nullptr);
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    int sock;
+    SSL_CTX* ctx;
+
+    ctx = create_context();
+    if (!ctx) {
+        exit(EXIT_FAILURE);
+    }
+
+    bool result;
+    result = configure_context(ctx);
+    if (!result) {
+        SSL_CTX_free(ctx);
+        exit(EXIT_FAILURE);
+    }
+
+    sock = create_socket(8080);
+    if (!sock) {
+        SSL_CTX_free(ctx);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        struct sockaddr_in addr;
+        int len = sizeof(addr);
+        SSL* ssl;
+
+        char buf[1024];
+        string wyniki;
+        int bytes;
+
+        int client = accept(sock, (struct sockaddr*)&addr, &len);
+        if (client >= 0)
+        {
+            cout << "Connection accepted" << endl;
+        }
+        if (client < 0) {
+            perror("Unable to accept");
+            continue;
+        }
+
+        ssl = SSL_new(ctx);
+        if (!ssl) {
+            perror("Error when creating SSL");
+            continue;
+        }
+
+        int res;
+        res = SSL_set_fd(ssl, client);
+        if (!res) {
+            perror("Error when connecting SSL object with a file descriptor");
+            continue;
+        }
+
+        int rozmiar = 0;
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+        }
+        else {
+            do
+            {
+                bytes = SSL_read(ssl, buf, 1023); //tu bedzie sie wykonywalo az SSL_read zwróci zero i wtedy jest break oraz również ograniczenie w while 
+                if (bytes > 0)
+                {
+                    buf[bytes] = 0;
+                    printf("Data: \"%s\"\n", buf);
+                    wyniki.append(buf, bytes);
+                }
+                else if (bytes == 0)
+                {
+                    printf("No data to receive\n"); // tutaj nie wiem ocb
+                    break;
+                }
+                else
+                {
+                    perror("Unable to read");
+                    break;
+                }
+            } while (bytes > 0);
+
+        }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        closesocket(client);
+    }
+
+    SSL_CTX_free(ctx);
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
